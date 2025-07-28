a085f10c6c66eea622dc1eb11b5bc69bdbbe2a52
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-07-28 18:45:39 -0300

    Layout, Header, Footer components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/components/layout/layout.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',
+    component: LayoutComponent,
+    children: [
+        { path: '', component: HomeComponent},
+    ]
+},
+ { path: '**', redirectTo: '' }
+];
