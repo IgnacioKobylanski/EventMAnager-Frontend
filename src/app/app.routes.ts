a633aa4e4e7feb9b23e2d9e9e755edfcca3212b8
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-08-19 18:09:44 -0300

    event detail component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './core/components/layout/layout.component';
 import { HomeComponent } from './features/home/home.component';
+import { EventDetailComponent } from './features/events/components/event-detail/event-detail.component';
 
 export const routes: Routes = [
     {path: '',
     component: LayoutComponent,
     children: [
         { path: '', component: HomeComponent},
+        { path: 'event/:id', component: EventDetailComponent}
     ]
 },
  { path: '**', redirectTo: '' }
--- src/app/features/events/components/event-card/event-card.component.ts
+++ src/app/features/events/components/event-card/event-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Event as AppEvent } from '../../models/Event';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './event-card.component.html',
   styleUrls: ['./event-card.component.scss']
 })
